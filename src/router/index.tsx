870970a1e3633c4168043cd8d6ce1bcab3ed1c50
luyao <maluyao0310@gmail.com>
2026-05-02 19:39:55 +0800

    update

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import AppLayout from '../layout/AppLayout'
 import SyncAzzi001Page from '../pages/SyncAzzi001Page'
 import GenAooi200Page from '../pages/GenAooi200Page'
@@ -6,38 +6,17 @@ import SyncAooi200Page from '../pages/SyncAooi200Page'
 import ParamDiffPage from '../pages/ParamDiffPage'
 import SettingsPage from '../pages/SettingsPage'
 
-function PageCache() {
-  const location = useLocation()
-
-  const pages = [
-    { path: '/sync-azzi001', element: <SyncAzzi001Page /> },
-    { path: '/gen-aooi200', element: <GenAooi200Page /> },
-    { path: '/sync-aooi200', element: <SyncAooi200Page /> },
-    { path: '/param-diff', element: <ParamDiffPage /> },
-    { path: '/settings', element: <SettingsPage /> },
-  ]
-
-  return (
-    <>
-      {pages.map((page) => (
-        <div
-          key={page.path}
-          style={{ display: location.pathname === page.path ? 'contents' : 'none' }}
-        >
-          {page.element}
-        </div>
-      ))}
-    </>
-  )
-}
-
 export default function AppRouter() {
   return (
     <HashRouter>
       <AppLayout>
         <Routes>
           <Route path="/" element={<Navigate to="/sync-azzi001" replace />} />
-          <Route path="/*" element={<PageCache />} />
+          <Route path="/sync-azzi001" element={<SyncAzzi001Page />} />
+          <Route path="/gen-aooi200" element={<GenAooi200Page />} />
+          <Route path="/sync-aooi200" element={<SyncAooi200Page />} />
+          <Route path="/param-diff" element={<ParamDiffPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
         </Routes>
       </AppLayout>
     </HashRouter>
